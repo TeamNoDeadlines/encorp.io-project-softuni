be6ef60645d9c015e24ed23877bac55238f5a7b9
ToniAndreev <Toni.Andreev@quantive.com>
2023-10-28 00:02:18 +0300

    update

--- SoftUniFest/Angular/softuni-project-2023-master/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    stripe: 'pk_test_51O5kcKImUtT79mvB9B1LHjqcFe946FvExwYzLjipTHGIZY0XFYxQxCTkONLxeFM399hfbFypfvIo6eCFde5yRKvo00eL4q6Jj4',
-    serverUrl: '/api',
-  };
