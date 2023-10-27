46624c6ee983de7da86b1d894bef1f815079df78
Desislava Todorova <desislava.todorova@quantive.com>
2023-10-27 13:43:32 +0300

    First commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
     production: false,
-
     stripe: 'pk_test_51O5kcKImUtT79mvB9B1LHjqcFe946FvExwYzLjipTHGIZY0XFYxQxCTkONLxeFM399hfbFypfvIo6eCFde5yRKvo00eL4q6Jj4',
     serverUrl: '/api',
   };
