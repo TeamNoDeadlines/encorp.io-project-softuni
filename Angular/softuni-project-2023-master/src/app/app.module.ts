52e1f57030ad31c669e9953f2998481979a74b6f
Desislava Todorova <desislava.todorova@quantive.com>
2023-10-28 12:33:25 +0300

    adding client dash, nav, footer, prod. service

--- Angular/softuni-project-2023-master/src/app/app.module.ts
+++ Angular/softuni-project-2023-master/src/app/app.module.ts
@@ -2,29 +2,14 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CancelComponent } from './cancel/cancel.component';
 import { SuccessComponent } from './success/success.component';
 import { CheckoutComponent } from './checkout/checkout.component';
-<<<<<<< HEAD:Angular/softuni-project-2023-master/src/app/app.module.ts
-=======
 import { ClientDashboardComponent } from './client-dashboard/client-dashboard.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { LoginComponent } from './login/login.component';
-import { MatIconModule } from '@angular/material/icon';
 import { NavComponent } from './nav/nav.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldDefaultOptions } from '@angular/material/form-field';
-import { MatFormFieldAppearance } from '@angular/material/form-field';
-import { ThemePalette } from '@angular/material/core';
 import { FooterComponent } from './footer/footer.component';
-
->>>>>>> 95a2d7c (adding client dash, nav, footer, prod. service):src/app/app.module.ts
 
 @NgModule({
   declarations: [
@@ -32,36 +17,12 @@ import { FooterComponent } from './footer/footer.component';
     CancelComponent,
     SuccessComponent,
     CheckoutComponent,
-<<<<<<< HEAD:Angular/softuni-project-2023-master/src/app/app.module.ts
-=======
     ClientDashboardComponent,
-    LoginComponent,
     FooterComponent,
-
-
->>>>>>> 95a2d7c (adding client dash, nav, footer, prod. service):src/app/app.module.ts
+    NavComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    // add these modules
-    MatCardModule,
-    MatButtonModule,
-    HttpClientModule,
-<<<<<<< HEAD:Angular/softuni-project-2023-master/src/app/app.module.ts
-=======
-    MatToolbarModule,
-    MatIconModule,
-    NavComponent,
-    FormsModule, 
-    ReactiveFormsModule,
-MatFormFieldModule,
-
-
->>>>>>> 95a2d7c (adding client dash, nav, footer, prod. service):src/app/app.module.ts
-  ],
-  providers: [ {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'outline'}}],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, HttpClientModule],
   bootstrap: [AppComponent],
+
 })
 export class AppModule {}
--- Angular/softuni-project-2023-master/src/app/client-dashboard/client-dashboard.component.ts
+++ Angular/softuni-project-2023-master/src/app/client-dashboard/client-dashboard.component.ts
@@ -2,7 +2,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CompanyService } from '../company.service';
 import { ProductService } from '../product.service';
-import {Product} from '../models/product';
+import { Product } from '../models/product';
+import { HttpClient } from '@angular/common/http';
+import { loadStripe } from '@stripe/stripe-js';
+import { environment } from '../../environments/environment';
+
 @Component({
   selector: 'app-client-dashboard',
   templateUrl: './client-dashboard.component.html',
@@ -13,14 +17,42 @@ export class ClientDashboardComponent {
   companies: any[] = [];
   selectedCompany: any;
   products: Product[] = [];
+  stripePromise = loadStripe(environment.stripe);
+
   constructor(
+    private http: HttpClient,
     private companyService: CompanyService,
     private productService: ProductService
   ) {}
 
+  async pay(): Promise<void> {
+    // here we create a payment object
+    const payment = {
+      name: 'Iphone',
+      currency: 'usd',
+      // amount on cents *10 => to be on dollar
+      amount: 99900,
+      quantity: '1',
+      cancelUrl: 'http://localhost:4200/cancel',
+      successUrl: 'http://localhost:4200/success',
+    };
+
+    const stripe = await this.stripePromise;
+
+    // this is a normal http calls for a backend api
+    this.http
+      .post(`${environment.serverUrl}/payment`, payment)
+      .subscribe((data: any) => {
+        // I use stripe to redirect To Checkout page of Stripe platform
+        stripe?.redirectToCheckout({
+          sessionId: data.id,
+        });
+      });
+  }
+  
   ngOnInit() {
     this.products = this.productService.getProducts();
-    console.log(this.products)
+    console.log(this.products);
   }
   searchCompanies(searchTerm: string) {
     // Implement search logic using the company service
