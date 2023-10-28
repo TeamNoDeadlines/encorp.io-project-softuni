7d604778ec838cd5c17272d58c9fd9773da90362
Desislava Todorova <desislava.todorova@quantive.com>
2023-10-28 14:32:01 +0300

    removing imports of removed component

--- Angular/softuni-project-2023-master/src/app/app.module.ts
+++ Angular/softuni-project-2023-master/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CancelComponent } from './cancel/cancel.component';
 import { SuccessComponent } from './success/success.component';
-import { CheckoutComponent } from './checkout/checkout.component';
 import { ClientDashboardComponent } from './client-dashboard/client-dashboard.component';
 import { NavComponent } from './nav/nav.component';
 import { FooterComponent } from './footer/footer.component';
@@ -16,7 +15,6 @@ import { FooterComponent } from './footer/footer.component';
     AppComponent,
     CancelComponent,
     SuccessComponent,
-    CheckoutComponent,
     ClientDashboardComponent,
     FooterComponent,
     NavComponent
