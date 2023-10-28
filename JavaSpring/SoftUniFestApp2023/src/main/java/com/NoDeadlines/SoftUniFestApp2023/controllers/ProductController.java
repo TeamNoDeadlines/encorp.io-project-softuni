48dd8811977715a99452224fc6f345fe79489019
ToniAndreev <Toni.Andreev@quantive.com>
2023-10-28 21:51:55 +0300

    typo fix

--- JavaSpring/SoftUniFestApp2023/src/main/java/com/NoDeadlines/SoftUniFestApp2023/controllers/ProductController.java
+++ JavaSpring/SoftUniFestApp2023/src/main/java/com/NoDeadlines/SoftUniFestApp2023/controllers/ProductController.java
@@ -1,10 +1,8 @@
 package com.NoDeadlines.SoftUniFestApp2023.controllers;
 
-import com.NoDeadlines.SoftUniFestApp2023.models.BusinessUser;
 import com.NoDeadlines.SoftUniFestApp2023.models.Product;
 import com.NoDeadlines.SoftUniFestApp2023.models.mapper.ModelMapper;
 import com.NoDeadlines.SoftUniFestApp2023.services.contracts.ProductService;
-import com.NoDeadlines.SoftUniFestApp2023.services.contracts.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,13 +10,13 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-public class ProductCntroller {
+public class ProductController {
     private final ModelMapper mapper;
 
 
     private final ProductService service;
 @Autowired
-    public ProductCntroller(ModelMapper mapper, ProductService service) {
+    public ProductController(ModelMapper mapper, ProductService service) {
         this.mapper = mapper;
         this.service = service;
     }
