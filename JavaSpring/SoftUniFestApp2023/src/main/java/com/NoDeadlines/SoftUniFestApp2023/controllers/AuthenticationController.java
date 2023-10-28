16d105a6f21ae2b78d7b3589d3e4d6f49c926023
ToniAndreev <Toni.Andreev@quantive.com>
2023-10-28 15:28:18 +0300

    Fix authentication

--- JavaSpring/SoftUniFestApp2023/src/main/java/com/NoDeadlines/SoftUniFestApp2023/controllers/AuthenticationController.java
+++ JavaSpring/SoftUniFestApp2023/src/main/java/com/NoDeadlines/SoftUniFestApp2023/controllers/AuthenticationController.java
@@ -1,106 +1,106 @@
-package com.web.project.webprojecttelerik.controllers;
-
-import com.web.project.webprojecttelerik.exceptions.ForbiddenActionException;
-import com.web.project.webprojecttelerik.exceptions.UnauthorizedOperationException;
-
-
-import com.web.project.webprojecttelerik.models.UserSession;
-import com.web.project.webprojecttelerik.models.dtos.UserDto;
-import com.web.project.webprojecttelerik.services.contracts.AuthenticationService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
-
-@RestController
-@RequestMapping("api/authentication")
-public class AuthenticationController {
-
-    private final RestTemplate restTemplate;
-
-    private final AuthenticationService service;
-
-    @Autowired
-    public AuthenticationController(AuthenticationService service){
-        this.service = service;
-        this.restTemplate = new RestTemplate();
-    }
-
-    /**
-     * Loges User in and returns a UserSession uuid
-     * @param userInfo Authorization header, format "username password"
-     * @return UserSession uuid field or HttpStatus 403
-     */
-    @GetMapping("/login")
-    public UserSession login(@RequestHeader(HttpHeaders.AUTHORIZATION) String userInfo){
-
-        String username = userInfo.split(" ")[0];
-        String password = userInfo.split(" ")[1];
-        try{
-            return service.login(username, password);
-        }catch (UnauthorizedOperationException e){
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, e.getMessage());
-        }
-
-    }
-
-    /**
-     * Logout User, deletes UserSession associated with set User
-     * @param uuid uuid of user to log out (UserSession uuid)
-     * @return String or HttpStatus 403
-     */
-    @GetMapping("/logout")
-    public String logout(@RequestHeader(HttpHeaders.AUTHORIZATION) String uuid){
-        try{
-            service.logout(uuid);
-            return "Logged out";
-        }catch (ForbiddenActionException | UnauthorizedOperationException e){
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, e.getMessage());
-        }
-    }
-
-    /**
-     * Creates new user
-     * @param userDto json with register data
-     * @return String "User created"
-     */
-    @PostMapping("/register")
-    public String register(@RequestBody UserDto userDto){
-        String url = "http://localhost:8080/api/users/";
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-
-        Map<String, Object> map = new HashMap<>();
-
-        map.put("username", userDto.getUsername());
-        map.put("password", userDto.getPassword());
-        map.put("firstName", userDto.getFirstName());
-        map.put("lastName", userDto.getLastName());
-        map.put("email", userDto.getEmail());
-        map.put("phoneNumber", userDto.getPhoneNumber());
-        map.put("admin", userDto.isAdmin());
-
-        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(map, headers);
-
-        ResponseEntity<UserDto> response = restTemplate.postForEntity(url, entity, UserDto.class);
-
-        if (response.getStatusCode() == HttpStatus.OK) {
-            return "User created";
-        } else {
-            return response.toString();
-        }
-
-    }
-
-
-
-
-}
+//package com.web.project.webprojecttelerik.controllers;
+//
+//import com.web.project.webprojecttelerik.exceptions.ForbiddenActionException;
+//import com.web.project.webprojecttelerik.exceptions.UnauthorizedOperationException;
+//
+//
+//import com.web.project.webprojecttelerik.models.UserSession;
+//import com.web.project.webprojecttelerik.models.dtos.UserDto;
+//import com.web.project.webprojecttelerik.services.contracts.AuthenticationService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.*;
+//import org.springframework.web.bind.annotation.*;
+//import org.springframework.web.client.RestTemplate;
+//import org.springframework.web.server.ResponseStatusException;
+//
+//import java.util.Collections;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//
+//@RestController
+//@RequestMapping("api/authentication")
+//public class AuthenticationController {
+//
+//    private final RestTemplate restTemplate;
+//
+//    private final AuthenticationService service;
+//
+//    @Autowired
+//    public AuthenticationController(AuthenticationService service){
+//        this.service = service;
+//        this.restTemplate = new RestTemplate();
+//    }
+//
+//    /**
+//     * Loges User in and returns a UserSession uuid
+//     * @param userInfo Authorization header, format "username password"
+//     * @return UserSession uuid field or HttpStatus 403
+//     */
+//    @GetMapping("/login")
+//    public UserSession login(@RequestHeader(HttpHeaders.AUTHORIZATION) String userInfo){
+//
+//        String username = userInfo.split(" ")[0];
+//        String password = userInfo.split(" ")[1];
+//        try{
+//            return service.login(username, password);
+//        }catch (UnauthorizedOperationException e){
+//            throw new ResponseStatusException(HttpStatus.FORBIDDEN, e.getMessage());
+//        }
+//
+//    }
+//
+//    /**
+//     * Logout User, deletes UserSession associated with set User
+//     * @param uuid uuid of user to log out (UserSession uuid)
+//     * @return String or HttpStatus 403
+//     */
+//    @GetMapping("/logout")
+//    public String logout(@RequestHeader(HttpHeaders.AUTHORIZATION) String uuid){
+//        try{
+//            service.logout(uuid);
+//            return "Logged out";
+//        }catch (ForbiddenActionException | UnauthorizedOperationException e){
+//            throw new ResponseStatusException(HttpStatus.FORBIDDEN, e.getMessage());
+//        }
+//    }
+//
+//    /**
+//     * Creates new user
+//     * @param userDto json with register data
+//     * @return String "User created"
+//     */
+//    @PostMapping("/register")
+//    public String register(@RequestBody UserDto userDto){
+//        String url = "http://localhost:8080/api/users/";
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setContentType(MediaType.APPLICATION_JSON);
+//
+//        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+//
+//        Map<String, Object> map = new HashMap<>();
+//
+//        map.put("username", userDto.getUsername());
+//        map.put("password", userDto.getPassword());
+//        map.put("firstName", userDto.getFirstName());
+//        map.put("lastName", userDto.getLastName());
+//        map.put("email", userDto.getEmail());
+//        map.put("phoneNumber", userDto.getPhoneNumber());
+//        map.put("admin", userDto.isAdmin());
+//
+//        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(map, headers);
+//
+//        ResponseEntity<UserDto> response = restTemplate.postForEntity(url, entity, UserDto.class);
+//
+//        if (response.getStatusCode() == HttpStatus.OK) {
+//            return "User created";
+//        } else {
+//            return response.toString();
+//        }
+//
+//    }
+//
+//
+//
+//
+//}
--- JavaSpring/SoftUniFestApp2023/src/main/java/com/NoDeadlines/SoftUniFestApp2023/services/AuthenticationServiceImpl.java
+++ JavaSpring/SoftUniFestApp2023/src/main/java/com/NoDeadlines/SoftUniFestApp2023/services/AuthenticationServiceImpl.java
@@ -1,71 +1,71 @@
-package com.web.project.webprojecttelerik.services;
-
-import com.web.project.webprojecttelerik.exceptions.ForbiddenActionException;
-import com.web.project.webprojecttelerik.exceptions.UnauthorizedOperationException;
-import com.web.project.webprojecttelerik.models.User;
-import com.web.project.webprojecttelerik.models.UserSession;
-import com.web.project.webprojecttelerik.repositories.contratcts.AuthenticationRepository;
-import com.web.project.webprojecttelerik.repositories.contratcts.UserRepository;
-import com.web.project.webprojecttelerik.services.contracts.AuthenticationService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-
-import javax.persistence.NoResultException;
-import java.sql.*;
-import java.time.Instant;
-
-@Service
-public class AuthenticationServiceImpl implements AuthenticationService {
-
-    private final AuthenticationRepository repository;
-    private final UserRepository userRepository;
-
-    @Autowired
-    public AuthenticationServiceImpl(AuthenticationRepository repository, UserRepository userRepository){
-        this.repository = repository;
-        this.userRepository = userRepository;
-    }
-
-    @Override
-    public UserSession login(String username, String password) {
-
-        User user = userRepository.get(username);
-        if(!user.getPassword().equals(password))
-            throw new UnauthorizedOperationException("Password mismatch");
-        UserSession session = repository.getSession(user.getId());
-        Timestamp currentTime = Timestamp.from(Instant.now());
-
-        if(currentTime.after(session.getTimestamp())){
-            repository.deleteSession(session);
-            return repository.getSession(user.getId());
-        }
-        return session;
-    }
-
-    @Override
-    public void logout(String uuid) {
-        try{
-            UserSession userSession = repository.getSession(uuid);
-            repository.deleteSession(userSession);
-        }catch (NoResultException e){
-            throw new ForbiddenActionException("User with session id " + uuid + "not logged in" );
-        }
-    }
-
-    @Override
-    public User getUserFromUUID(String uuid) {
-        UserSession userSession = repository.getSession(uuid);
-        Timestamp currentTime = Timestamp.from(Instant.now());
-
-        if(currentTime.after(userSession.getTimestamp()))
-        {
-            repository.deleteSession(userSession);
-            throw new UnauthorizedOperationException("Session expired, please login");
-        }
-
-        return userRepository.get(userSession.getUser());
-    }
-
-
-}
+//package com.web.project.webprojecttelerik.services;
+//
+//import com.web.project.webprojecttelerik.exceptions.ForbiddenActionException;
+//import com.web.project.webprojecttelerik.exceptions.UnauthorizedOperationException;
+//import com.web.project.webprojecttelerik.models.User;
+//import com.web.project.webprojecttelerik.models.UserSession;
+//import com.web.project.webprojecttelerik.repositories.contratcts.AuthenticationRepository;
+//import com.web.project.webprojecttelerik.repositories.contratcts.UserRepository;
+//import com.web.project.webprojecttelerik.services.contracts.AuthenticationService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//
+//import javax.persistence.NoResultException;
+//import java.sql.*;
+//import java.time.Instant;
+//
+//@Service
+//public class AuthenticationServiceImpl implements AuthenticationService {
+//
+//    private final AuthenticationRepository repository;
+//    private final UserRepository userRepository;
+//
+//    @Autowired
+//    public AuthenticationServiceImpl(AuthenticationRepository repository, UserRepository userRepository){
+//        this.repository = repository;
+//        this.userRepository = userRepository;
+//    }
+//
+//    @Override
+//    public UserSession login(String username, String password) {
+//
+//        User user = userRepository.get(username);
+//        if(!user.getPassword().equals(password))
+//            throw new UnauthorizedOperationException("Password mismatch");
+//        UserSession session = repository.getSession(user.getId());
+//        Timestamp currentTime = Timestamp.from(Instant.now());
+//
+//        if(currentTime.after(session.getTimestamp())){
+//            repository.deleteSession(session);
+//            return repository.getSession(user.getId());
+//        }
+//        return session;
+//    }
+//
+//    @Override
+//    public void logout(String uuid) {
+//        try{
+//            UserSession userSession = repository.getSession(uuid);
+//            repository.deleteSession(userSession);
+//        }catch (NoResultException e){
+//            throw new ForbiddenActionException("User with session id " + uuid + "not logged in" );
+//        }
+//    }
+//
+//    @Override
+//    public User getUserFromUUID(String uuid) {
+//        UserSession userSession = repository.getSession(uuid);
+//        Timestamp currentTime = Timestamp.from(Instant.now());
+//
+//        if(currentTime.after(userSession.getTimestamp()))
+//        {
+//            repository.deleteSession(userSession);
+//            throw new UnauthorizedOperationException("Session expired, please login");
+//        }
+//
+//        return userRepository.get(userSession.getUser());
+//    }
+//
+//
+//}
